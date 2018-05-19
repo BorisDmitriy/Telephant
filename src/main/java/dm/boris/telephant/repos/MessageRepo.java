8a58ec83e1407f28548e7023c397181b8452cdbc
Boris Dmitriy <BorisDmitriy@users.noreply.github.com>
2018-05-19 20:07:33 +0300

    database connection

---
+++ src/main/java/dm/boris/telephant/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package dm.boris.telephant.repos;
+
+import dm.boris.telephant.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
